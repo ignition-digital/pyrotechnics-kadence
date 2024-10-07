cd2ed3af20dbb71afc861499353b4e5b68dbe415
Presley <tph.presley@gmail.com>
2024-10-07 15:56:01 +0800

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("if (false) {}\r\n\n\n//# sourceURL=webpack://golden-era-world-webpack/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://golden-era-world-webpack/./src/index.js?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+// Define your 'roots' variable
+//const ROOTS_URL = "";
+const ROOTS_URL = "https://ignition-digital.github.io/goldeneraworld";
+
 const pages = [
   { name: "index", template: "./src/index.html" },
   { name: "about-us", template: "./src/about-us.html" },
@@ -11,9 +15,14 @@ const pages = [
   { name: "contact-us", template: "./src/contact-us.html" },
   { name: "faq", template: "./src/faq.html" },
   { name: "coming-soon", template: "./src/coming-soon.html" },
+  { name: "404", template: "./src/404.html" },
   { name: "info-adjacent", template: "./src/info-adjacent.html" },
   { name: "property-slider", template: "./src/property-slider.html" },
   { name: "project-complex", template: "./src/project-complex.html" },
+  { name: "apartment-single", template: "./src/apartment-single.html" },
+  { name: "feature-bellow", template: "./src/feature-bellow.html" },
+  { name: "info-bellow", template: "./src/info-bellow.html" },
+  { name: "landing", template: "./src/landing.html" },
 ];
 
 const htmlPlugins = pages.map(
@@ -27,14 +36,24 @@ const htmlPlugins = pages.map(
       },
       inject: "head",
       templateParameters: {
-        commonHead: fs.readFileSync(
-          path.resolve(__dirname, "src", "components", "common-head.html"),
-          "utf-8"
-        ),
+        commonHead: fs
+          .readFileSync(
+            path.resolve(__dirname, "src", "components", "common-head.html"),
+            "utf-8"
+          )
+          .replace(/<%= roots %>/g, ROOTS_URL),
+        // Add roots to template parameters
+        roots: ROOTS_URL,
       },
-      navbar: fs.readFileSync("./src/components/navbar.html", "utf8"),
-      footer: fs.readFileSync("./src/components/footer.html", "utf8"),
-      bottom: fs.readFileSync("./src/components/bottom-toolbar.html", "utf8"),
+      navbar: fs
+        .readFileSync("./src/components/navbar.html", "utf8")
+        .replace(/<%= roots %>/g, ROOTS_URL),
+      footer: fs
+        .readFileSync("./src/components/footer.html", "utf8")
+        .replace(/<%= roots %>/g, ROOTS_URL),
+      bottom: fs
+        .readFileSync("./src/components/bottom-toolbar.html", "utf8")
+        .replace(/<%= roots %>/g, ROOTS_URL),
     })
 );
 
@@ -50,7 +69,7 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "dist"),
     },
-    watchFiles: ["src/**/*.html", "src/**/*.js", "src/**/*.css"], // Add more file types if needed
+    watchFiles: ["src/**/*.html", "src/**/*.js", "src/**/*.css"],
     hot: true,
     port: 8080,
     open: true, // Opens the browser automatically
