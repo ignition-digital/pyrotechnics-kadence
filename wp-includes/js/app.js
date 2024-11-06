57ad2cafb92a18e6804b1fc5444c657cdfab4831
Presley <tph.presley@gmail.com>
2024-11-07 07:25:37 +0800

    update product listing

--- wp-includes/js/app.js
+++ wp-includes/js/app.js
@@ -1,6 +1,23 @@
-function fetchAllProducts() {
-  const inventoryProductsUrl = "data/inventory.json";
-  return fetchProductsFromJsonFile(inventoryProductsUrl);
+async function fetchAllProducts() {
+  const cacheName = "product-cache";
+  const version = 1.3;
+  const inventoryProductsUrl = `data/inventory.json?v=${version}`;
+
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(inventoryProductsUrl);
+
+  if (cachedResponse) {
+    return cachedResponse.json();
+  } else {
+    const networkResponse = await fetchProductsFromJsonFile(
+      inventoryProductsUrl
+    );
+    cache.put(
+      inventoryProductsUrl,
+      new Response(JSON.stringify(networkResponse))
+    );
+    return networkResponse;
+  }
 }
 
 function fetchProductsFromJsonFile(jsonFileUrl) {
@@ -28,6 +45,9 @@ function renderProductList(products) {
     var productHTML = generateProductHTML(product);
     productContainer.innerHTML += productHTML;
   });
+
+  // Trigger reflow to ensure styles are applied
+  productContainer.offsetHeight;
 }
 
 function generateProductHTML(product) {
